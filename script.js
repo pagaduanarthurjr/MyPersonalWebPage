ae0d11e4ef905796e0258afc795a35039fd7f5d1
pagaduanarthurjr <eiinkimura@gmail.com>
2024-09-15 16:55:31 +0800

    Added files
    Personal Webpage files

---
+++ script.js
@@ -0,0 +1,8 @@
+function initGallery() {
+    console.log("Gallery initialized");
+}
+document.addEventListener('DOMContentLoaded', () => {
+    console.log("Welcome to Arthur's website!");
+    initGallery();
+});
+
